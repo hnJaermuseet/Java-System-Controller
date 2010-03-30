8d8049cb1e071024e6b800b9a43259db9ba3ac76
hnJaermuseet <hn@jaermuseet.no>
2010-03-30 14:29:07 +0200

    Rename

---
+++ jsc_controller/BasicMenuItem.java
@@ -0,0 +1,8 @@
+package jsc_controller;
+
+public class BasicMenuItem extends jsc_server.MenuItem {
+
+	public BasicMenuItem (String tekst) {
+		this.elementTxt = tekst;
+	}
+}
--- jsc_controller/MenuItem.java
+++
@@ -1,8 +0,0 @@
-package jsc_controller;
-
-public class MenuItem extends jsc_server.MenyElement {
-
-	public MenuItem (String tekst) {
-		this.elementTxt = tekst;
-	}
-}
