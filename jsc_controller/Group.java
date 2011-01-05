e93e890e21140b40630e0cfe3bd66bd7b80d216b
hnJaermuseet <hn@jaermuseet.no>
2011-01-05 15:28:36 +0100

    Group has statusText.

--- jsc_controller/Group.java
+++ jsc_controller/Group.java
@@ -2,6 +2,8 @@ package jsc_controller;
 
 import java.util.ArrayList;
 
+import translation.T;
+
 import jsc_server.CantFindMachine;
 import jsc_server.Machine;
 import jsc_server.MenuItem;
@@ -45,7 +47,7 @@ public class Group extends MenuItem {
 	}
 
 	public String toString () {
-		return name;
+		return name + " ("+getStatusText()+")";
 	}
 	
 	public void wakeup () {
@@ -70,15 +72,146 @@ public class Group extends MenuItem {
 		return "Valgt: gruppen " + this.toString();
 	}
 
-	@Override
-	public int getStatus() {
-		// TODO Auto-generated method stub
-		return 0;
+	public int[] getStatusArray() {
+		int found[] = new int[10];
+		found[0] = 0;
+		found[1] = 0;
+		found[2] = 0;
+		found[3] = 0;
+		found[4] = 0;
+		found[5] = 0;
+		found[6] = 0;
+		found[7] = 0;
+		found[8] = 0;
+		found[9] = 0;
+		
+		int status;
+		for (MenuItem item : group_items) {
+			status = item.getStatus();
+			
+			switch(status)
+			{
+				case 1: // ping_ok
+				case 2: // ikke_ping
+				case 3: // restart_sendt
+				case 4: // restart_mottatt
+				case 5: // shutdown_sendt
+				case 6: // shutdown_mottatt
+				case 7: // oppstart_sendt
+				case 8: // error
+				case 9: // offline
+					found[status] ++;
+				default: // unknown
+					found[0]++;
+			}
+		}
+		
+		return found;
 	}
 
 	@Override
 	public String getStatusText() {
+		int[] status = getStatusArray();
+		
+		String msg = "";
+		boolean earliermsg = false;
+		
+		// Online
+		if(status[1] > 0)
+		{
+			if(status[1] == group_items.size())
+				msg += T.t("All is online");
+			else
+			{
+				msg += status[1] + " " + T.t("is online");
+				earliermsg = true;
+			}
+		}
+		
+		// Offline
+		int offline = status[2]+status[9];
+		if(offline > 0)
+		{
+			if(earliermsg)
+				msg += ", ";
+			
+			if(offline == group_items.size())
+				msg += T.t("All is offline");
+			else
+			{
+				msg += offline + " " + T.t("is offline");
+				earliermsg = true;
+			}
+		}
+		
+		// Restart
+		int restarting = status[3]+status[4];
+		if(restarting > 0)
+		{
+			if(earliermsg)
+				msg += ", ";
+			
+			if(restarting == group_items.size())
+				msg += T.t("All is restarting");
+			else
+			{
+				msg += restarting + " " + T.t("is restarting");
+				earliermsg = true;
+			}
+		}
+		
+		// Shutdown
+		int shutingdown = status[5]+status[6];
+		if(shutingdown > 0)
+		{
+			if(earliermsg)
+				msg += ", ";
+			
+			if(shutingdown == group_items.size())
+				msg += T.t("All is shuting down");
+			else
+			{
+				msg += shutingdown + " " + T.t("is shuting down");
+				earliermsg = true;
+			}
+		}
+		
+		// Starting up
+		if(status[7] > 0)
+		{
+			if(earliermsg)
+				msg += ", ";
+			
+			if(status[7] == group_items.size())
+				msg += T.t("All is starting up");
+			else
+			{
+				msg += status[7] + " " + T.t("is starting up");
+				earliermsg = true;
+			}
+		}
+		
+		// Error
+		if(status[8] > 0)
+		{
+			if(earliermsg)
+				msg += ", ";
+			
+			if(status[8] == group_items.size())
+				msg += T.t("All has an error");
+			else
+			{
+				msg += status[8] + " " + T.t("has an error");
+				earliermsg = true;
+			}
+		}
+		
+		return msg;
+	}
+
+	@Override
+	public int getStatus() {
 		// TODO Auto-generated method stub
-		return null;
+		return 0;
 	}
 }
--- translation/Translator.java
+++ translation/Translator.java
@@ -6,8 +6,32 @@ public class Translator {
 	{
 		if(english.equals("All machines"))
 				return "Alle maskiner";
+		else if(english.equals("All is online"))
+			return "Alle er på";
+		else if(english.equals("is online"))
+			return "er på";
+		else if(english.equals("All is offline"))
+			return "Alle er av";
+		else if(english.equals("is offline"))
+			return "er av";
+		else if(english.equals("All is restarting"))
+			return "Alle restarter";
+		else if(english.equals("is restarting"))
+			return "restarter";
+		else if(english.equals("All is shuting down"))
+			return "Alle slås av";
+		else if(english.equals("is shuting down"))
+			return "slås av";
+		else if(english.equals("All is starting up"))
+			return "Alle starter opp";
+		else if(english.equals("is starting up"))
+			return "starter opp";
+		else if(english.equals("All has an error"))
+			return "Alle har en feil";
+		else if(english.equals("has an error"))
+			return "har en feil";
 		
-		System.out.println("Unknown translation");
+		System.out.println("Unknown translation for \""+english+"\"");
 		return english;
 	}
 }
