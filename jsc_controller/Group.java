910a3dca1d9c217bdf201862880c9a0462c8744b
hnJaermuseet <hn@jaermuseet.no>
2011-01-05 09:19:38 +0100

    Group is using ArrayList instead of array

--- jsc_controller/Group.java
+++ jsc_controller/Group.java
@@ -1,18 +1,21 @@
 package jsc_controller;
 
+import java.util.ArrayList;
+
 import jsc_server.CantFindMachine;
 import jsc_server.Machine;
 import jsc_server.MenuItem;
 
 public class Group extends MenuItem {
-	private String gruppe_navn;
-	private MenuItem[] gruppe_innhold = new MenuItem[4];
-	private int gruppe_num = 0;
+	// TODO: Make private again
+	public String gruppe_navn;
+	protected ArrayList<MenuItem> group_items;
 	
 	public Group (String gruppe_navn) {
+		group_items = new ArrayList<MenuItem>();
 		this.gruppe_navn = gruppe_navn;
 	}
-	
+	/*
 	public void addContent (String innhold_navn) {
 		if(this.gruppe_num == this.gruppe_innhold.length) {
 			MenuItem[] tmp = new MenuItem[gruppe_innhold.length * 2];
@@ -29,27 +32,14 @@ public class Group extends MenuItem {
 			System.out.println("Can't find config file for machine: " + innhold_navn);
 			gruppe_innhold[gruppe_num] = null;
 		}
-	}
+	}*/
 	
 	public void addContent (MenuItem maskin) {
-		if(this.gruppe_num == this.gruppe_innhold.length) {
-			MenuItem[] tmp = new MenuItem[gruppe_innhold.length * 2];
-			for (int i = 0; i < gruppe_innhold.length; i++) {
-				tmp[i] = gruppe_innhold[i];
-			}
-			gruppe_innhold = tmp;
-		}
-		
-		gruppe_innhold[gruppe_num] = maskin;
-		gruppe_num++;
+		group_items.add(maskin);
 	}
 	
-	public MenuItem[] getContent () {
-		return gruppe_innhold;
-	}
-	
-	public int getContentNum () {
-		return gruppe_num;
+	public ArrayList<MenuItem> getContent () {
+		return group_items;
 	}
 
 	public String toString () {
@@ -57,20 +47,20 @@ public class Group extends MenuItem {
 	}
 	
 	public void wakeup () {
-		for (int i = 0; i < gruppe_num; i++) {
-			gruppe_innhold[i].wakeup();
+		for (int i = 0; i < group_items.size(); i++) {
+			group_items.get(i).wakeup();
 		}
 	}
 	
 	public void shutdown () {
-		for (int i = 0; i < gruppe_num; i++) {
-			gruppe_innhold[i].shutdown();
+		for (int i = 0; i < group_items.size(); i++) {
+			group_items.get(i).shutdown();
 		}
 	}
 	
 	public void reboot () {
-		for (int i = 0; i < gruppe_num; i++) {
-			gruppe_innhold[i].reboot();
+		for (int i = 0; i < group_items.size(); i++) {
+			group_items.get(i).reboot();
 		}
 	}
 	
