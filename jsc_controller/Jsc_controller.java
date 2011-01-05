1aec027c09869494203db675f9e4bf6a1bf93db5
hnJaermuseet <hn@jaermuseet.no>
2011-01-05 15:36:10 +0100

    Changing window size to 500x500

--- jsc_controller/Jsc_controller.java
+++ jsc_controller/Jsc_controller.java
@@ -109,6 +109,7 @@ public class Jsc_controller extends JPanel {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(this);
 		frame.pack();
+		frame.setSize(500, 500);
 		frame.setVisible(true);
 		
 	}
